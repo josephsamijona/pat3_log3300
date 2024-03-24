18e0879cdd5e6d58038847f67c82b23170ccef7f
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-03-23 22:21:29 -0400

    delete

--- ecoles/ecole/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcoleConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "ecole"
--- ecoles/ecole/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-from .models import Etudiant
-
-def liste_etudiants(request):
-    etudiants = Etudiant.objects.all()
-    return render(request, 'index.html', {'etudiants': etudiants})
