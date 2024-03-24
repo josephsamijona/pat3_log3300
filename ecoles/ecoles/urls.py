447c3e4c43c702e9eaea0b7fe3c743f24cd7df9a
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-03-23 22:11:35 -0400

    2

--- ecoles/ecoles/urls.py
+++ ecoles/ecoles/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecoles project.
+URL configuration for project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,15 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
-
+from ecole import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
-
-from . import views
-
-urlpatterns = [
-    path('', views.liste_etudiants, name='liste_etudiants'),
+    path("listes/",views.affichage)
 ]
